691a2de0912aeadd6fc2b3cdaec0ae37a1aa6003
Alexandre Hartmann <alexandre.hartmann@o2obots.com>
2023-10-28 17:53:52 -0300

    feat: login page

---
+++ src/components/Forms/login/login-form.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { Input, Label, Helper, Button, Checkbox, A } from 'flowbite-svelte';
+  import logo from '$lib/images/logo-2.png';
+</script>
+
+<form method="POST" class="p-4 w-screen max-w-md">
+  <div class="mb-4 w-100 flex justify-center">
+    <img src={logo} alt="logo" class="h-auto w-48 rounded-full" />
+  </div>
+
+  <Label class="text-white" for="username">Username</Label>
+  <Input name="username" id="username" type="text" placeholder="Username" class="mt-2 mb-4" />
+
+  <Label class="text-white" for="email">Email</Label>
+  <Input name="email" id="email" type="email" placeholder="Email" class="mt-2 mb-4" />
+
+  <Label class="text-white" for="password">Password</Label>
+  <Input name="password" id="password" type="password" placeholder="Password" class="mt-2 mb-4" />
+
+  <div class="my-2 flex justify-end items-center flex-row-reverse">
+    <Label class="text-white" for="remember">Remember me</Label>
+    <Checkbox name="remember" id="remember" label="Remember me" />
+  </div>
+
+  <div class="w-100 my-4">
+    <Button type="submit" class="w-full">Login</Button>
+  </div>
+
+  <div class="mt-2">
+    <Helper>
+      <A href="#">Forgot password?</A>
+    </Helper>
+  </div>
+</form>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import LoginForm from '../components/Forms/login/login-form.svelte';
+  import backgroundImage from '$lib/images/logo.png';
+</script>
+
+<div class="bg-background min-h-screen h-full flex justify-center items-center md:justify-">
+  <div id="bg-div" class="hidden md:block">
+    <img src={backgroundImage} alt="background" class="md:h-screen md:w-100" />
+  </div>
+
+  <LoginForm />
+</div>
